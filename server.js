9159cffd0534f2d3bc34daba058497368f3c8a6e
Donald Joy <djsmacbookpro-2017@Donalds-MacBook-Pro.local>
2018-01-02 15:21:48 -0500

    mongoose config for deployment

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var bodyParser = require("body-parser");
 var logger = require("morgan");
 var mongoose = require("mongoose");
 var path = require("path");
+var exphbs = require("express-handlebars");
+
 
 // Our scraping tools
 // Axios is a promised-based http library, similar to jQuery's Ajax method
@@ -27,10 +29,16 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // Use express.static to serve the public folder as a static directory
 app.use(express.static("public"));
 
+//Handlebars
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
 // Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
-mongoose.connect("mongodb://localhost/djmongoscraper_db", {
+mongoose.connect("MONGODB_URI", {
   useMongoClient: true
 });
 
